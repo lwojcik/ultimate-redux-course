aa7a601f8df031b6347d8fe16ea0e45e8fc3efa0
Łukasz Wójcik <lwojcik@lukem.net>
2021-02-15 20:21:33 +0100

    Completed: Debugging Redux Applications

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,11 +1,13 @@
-import store from './customStore';
+import store from './store';
 import * as actions from './actions';
 
 store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded("Bug 1"));
+// store.dispatch(actions.bugAdded("Bug 1"));
+// store.dispatch(actions.bugAdded("Bug 2"));
+// store.dispatch(actions.bugAdded("Bug 3"));
 
 console.log(store.getState());
 
